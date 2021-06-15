4fd184054966d9e87f282bbd01a017459b5663d2
Paweł Kowalczyk <76018570+pawelkowalczyk98@users.noreply.github.com>
2021-06-15 14:05:09 +0200

    Update codes.py

--- codes.py
+++ codes.py
@@ -1,11 +1,17 @@
 import pickle
-
-dic_service_code = {101: "Successfully Logged in",
+https://github.com/pawelkowalczyk98/File-upload-download-server/blob/main/codes.py
+dic_service_code = { # Login
+                    101: "Successfully Logged in",
                     102: "Successfully Logged out",
                     103: "Username available",
                     201: "Incorrect password",
                     202: "Incorrect username",
-                    203: "Username unavailable"}
+                    203: "Username unavailable",
+
+                    # FILE
+                    301: "Successfully. Server start sending file. ",
+                    401: "File not exist."
+                    }
 
 
 # dic_service_code BACKUP
