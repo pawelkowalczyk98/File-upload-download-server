e34ba6806ce39590f52879404002f6414c383fc2
Dominik Dudek <79716065+nikdudek@users.noreply.github.com>
2021-06-18 01:32:24 +0200

    18-06-2021
    Jeszcze będę edytował

--- client.py
+++ client.py
@@ -5,13 +5,21 @@ import socket
 import pickle
 import classes
 import codes
+import ssl
 import os
+from getpass import getpass
+
 # __________________________________________________________
 # GLOBAL VARIABLES
 USER = ""
 CRLF = b"\r\n\r\n"
+# Map SESSION_ID with USER at server site, for better safety
+SESSION_ID = 0
 # (Service action counter for safety - TO_DO)
 SERVICE_CNT = 0
+
+context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile='server.crt')
+context.load_cert_chain(certfile='client.crt', keyfile='client.key')
 
 
 # __________________________________________________________
@@ -39,18 +47,38 @@ def logout_service(server):
 def register_service(server):
     global USER
     USER = input("Please enter desired username (or leave empty to cancel operation): ")
-    # Must check if user contains only a-z and A-Z chars!
+    # Remove whitespaces
+    USER.strip()
     if USER == '':
         print("Operation cancelled.")
     else:
         code = username_check(USER, server)
         if code == 103:
             print(codes.dic_service_code[code])
-            # create user
+            pw = getpass("Enter password: ")
+            if getpass("Repeat password: ") != pw:
+                print(codes.dic_service_code[201])
+            else:
+                print("Encoded password: " + pw)
+                register_user_service(USER, pw, server)
+
         elif code == 203:
-            print(codes.dic_service_code[code] + "\nTry again? (y/n)")
-            if input("> ") == 'y':
-                register_service(server)
+            print(codes.dic_service_code[code])
+
+
+def register_user_service(user, pw, server):
+    data_register = classes.Register(user, pw)
+    data = classes.Main("register", len(data_register), data_register)
+    data_string = pickle.dumps(data)
+    s.sendall(data_string + CRLF)
+    receive(server)
+
+
+def exit_service(server):
+    data = classes.Main("exit", len(USER), USER)
+    data_string = pickle.dumps(data)
+    s.sendall(data_string + CRLF)
+
 
 def download_services(server): # poprawić size
     global USER
@@ -111,12 +139,11 @@ def send_services(server):
 
             print("\t\tCorrect send")
             f.close()
+
+
 # ____________________________________________________________
 # Funkcje pomocnicze
-
-def username_check(username, server): # hasło jeszce
-    # Czy muszą to być klasy? Mati mówił, że to i tak te same rodzaje wiadomości :/
-    # nie mam pojęcia tak wydawało mi się że tak będzie najprościej
+def username_check(username, server):
     data = classes.Main("username_check", len(username), username)
     data_string = pickle.dumps(data)
     s.sendall(data_string + CRLF)
@@ -149,36 +176,44 @@ def receive_service_code(server):  # zwraca kod, jezeli service_code
         return data_obj.content
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
-    s.connect(('localhost', 1769))
+    sock.connect(('localhost', 1769))
+    with context.wrap_socket(sock, server_hostname='localhost') as s:
+        print(s.version())
+        print('WELCOME TO FTP-CLIENT!\n')
 
-    while True:
-        cmd = input('> ')
+        while True:
 
-        if cmd == 'exit':
-            break
+            print('AVAILABLE COMMANDS: register, login, logout, help, exit\n')
+            cmd = input('> ')
 
-        elif cmd == 'help':
-            print('*Insert function description here*')
+            if cmd == 'exit':
+                exit_service(s)
+                break
 
-        elif cmd == 'login':
-            login_service(s)
+            elif cmd == 'help':
+                print('*Insert function description here*')
 
-        elif cmd == 'logout':
-            logout_service(s)
+            elif cmd == 'login':
+                login_service(s)
 
-        elif cmd == 'register':
-            register_service(s)
+            elif cmd == 'logout':
+                logout_service(s)
 
-        elif cmd == 'download':
-            download_services(s)
+            elif cmd == 'register':
+                register_service(s)
 
-        elif cmd == 'send':
-            send_services(s)
+            elif cmd == 'download':
+                download_services(s)
 
-        else:
-            print("Incorrect command")
+            elif cmd == 'send':
+                send_services(s)
+
+            else:
+                print("Incorrect command")
+
+        s.close()
 
 except socket.error:
     print('Error')
